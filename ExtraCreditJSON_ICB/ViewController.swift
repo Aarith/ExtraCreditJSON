c17822c2dca0ab326ef36f5f6eeafbe67c17a91d
Ian Campbell Brothers <iab15a@dmc-207-17.local>
2019-03-08 08:42:20 -0600

    Finished adding output to console

--- ExtraCreditJSON_ICB/ViewController.swift
+++ ExtraCreditJSON_ICB/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
+struct Country: Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var url = "https://restcountries.eu/rest/v2/all"
+        let url = "https://restcountries.eu/rest/v2/all"
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do {
+                var countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for country in countries {
+                    print(country.name + ": " + country.capital + " in " + country.region)
+                }
+            } catch {
+                print("We got an error")
+            }
+        }.resume()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 

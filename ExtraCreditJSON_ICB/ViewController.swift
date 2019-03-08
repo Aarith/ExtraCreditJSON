bd607fd6a0597b903b2ffbb926d6b0745788e325
Ian Campbell Brothers <iab15a@dmc-207-17.local>
2019-03-08 08:25:04 -0600

    Initial Commit

--- ExtraCreditJSON_ICB/ViewController.swift
+++ ExtraCreditJSON_ICB/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var url = "https://restcountries.eu/rest/v2/all"
     }
 
 
